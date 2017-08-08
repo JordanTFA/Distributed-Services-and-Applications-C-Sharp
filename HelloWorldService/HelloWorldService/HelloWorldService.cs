817845055cc3ee5eedcfa9d042fa5847d8f0e96c
JordanTFA <Jordantfa@hotmail.co.uk>
2017-08-08 23:20:56 +0100

    Initial Commit
    Still a lot to do on this topic

---
+++ HelloWorldService/HelloWorldService/HelloWorldService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ServiceModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldService
+{
+    class HelloWorldService : IHelloWorldService
+    {
+        public string HelloWorld(){
+            return "Hello World";
+        }
+    }
+}
---
+++ HelloWorldService/HelloWorldService/IHelloWorldService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ServiceModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldService
+{
+    [ServiceContract(Namespace="http://www.napier.ac.uk")]
+    interface IHelloWorldService
+    {
+        [OperationContract]
+        string HelloWorld();
+    }
+}
