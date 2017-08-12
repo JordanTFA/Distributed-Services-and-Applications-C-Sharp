d16a5a663da1f7228cab08774828daac9806c494
JordanTFA <Jordantfa@hotmail.co.uk>
2017-08-12 22:35:55 +0100

    Added example of DataContract

---
+++ RESTful/RESTful/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTful
+{
+    [DataContract]
+    class Student
+    {
+        [DataMember(Name = "matric_no")]
+        public string Matric;
+
+        [DataMember(Name = "name")]
+        public string Name;
+    }
+}
---
+++ RESTful/RESTful/Students.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTful
+{
+    [ServiceContract]
+    class Students
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "")]
+        public Student GetStudent()
+        {
+            Student student = new Student();
+            student.Matric = "01010101";
+            student.Name = "Bob Bobson";
+            return student;
+        }
+    }
+}
